241537af702a7a314ff1cfcc034d036afeb6c3ed
Tu Chung <tuthienchung@gmail.com>
2018-09-21 14:31:33 -0400

    navigation cont

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Navigation />
+    <List />
+  </div>
+
+</template>
+<script>
+import Navigation from './Navigation'
+// import List from './dash_board/List'
+export default {
+  name: 'DashBoard',
+  components: {
+    Navigation
+    // List
+  },
+  data: function () { 
+   // GET /someUrl
+      this.$http.get('https://jsonplaceholder.typicode.com/posts/1/comments').then(response => {
+
+          // get body data
+          console.log(response.body);
+        return this.someData = response.body;
+
+        }, response => {
+          // error callback
+        });
+  }
+  
+}
+</script>
---
+++ src/components/dash_board/List.vue
@@ -0,0 +1,28 @@
+<template>
+<div></div>  
+</template>
+
+<script>
+// import Note from './Note'
+export default {
+  name: 'List',
+  // components: {
+  //   Note
+  // },
+  "user" : getUser()
+   
+}
+
+function getUser(){
+   // GET /someUrl
+ this.$http.get('https://jsonplaceholder.typicode.com/posts/1/comments').then(response => {
+
+    // get body data
+    console.log(response.body);
+  return this.someData = response.body;
+
+  }, response => {
+    // error callback
+  });
+}
+</script>
